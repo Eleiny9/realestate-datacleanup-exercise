b505a1cf261be6af9e4207891f5ae4fdd09fe4d4
Eleiny9 <eleiny9@gmail.com>
2024-07-31 20:09:46 +0000

    cantidad de población level5

--- proyecto1.py
+++ proyecto1.py
@@ -46,7 +46,13 @@ casa_peque = datos.iloc[id_peque]
 print("La casa más pequeña tiene una superficie de :", casa_peque["surface"])
 print("y esta ubicada en: ", casa_peque["address"])
 
+# Cantidad de poblaciones level 5
 
+print("Ejercicio 4")
+
+level_5 = datos["level5"].unique().size
+
+print("la cantidad de población level 5:", level_5)
 
 
 
